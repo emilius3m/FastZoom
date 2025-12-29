f1879fd8b35446b8c3f491c9f4f50d1f3b2bc487
E3M <e3mcorp@gmail.com>
2025-12-29 17:48:53 +0100

    ocr optimization

--- app/core/logging.py
+++ app/core/logging.py
@@ -93,7 +93,7 @@ def setup_logging(
         console_format = (
             "<green>{time:HH:mm:ss}</green> | "
             "<level>{level: <8}</level> | "
-            "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> | "
+            "<cyan>{module}</cyan>:<cyan>{line}</cyan> | "
             "<level>{message}</level>"
         )
     
